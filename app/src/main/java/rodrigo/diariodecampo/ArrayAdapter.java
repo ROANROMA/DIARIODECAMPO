6dc7b71df94ea3bcd82044c4bf60ec9bab893e2b
rodrigo <rodrigoandres80@hotmail.com>
2016-03-06 14:57:38 -0500

    diario de campo

---
+++ app/src/main/java/rodrigo/diariodecampo/ArrayAdapter.java
@@ -0,0 +1,7 @@
+package rodrigo.diariodecampo;
+
+/**
+ * Created by Administrador on 04/03/2016.
+ */
+public class ArrayAdapter {
+}
---
+++ app/src/main/java/rodrigo/diariodecampo/DCAMPO.java
@@ -0,0 +1,21 @@
+package rodrigo.diariodecampo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class DCAMPO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dcampo);
+
+
+
+
+
+    }
+
+
+}
