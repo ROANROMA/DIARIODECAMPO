4e7163674ec11899f8408bc2f1d03dde4b8d6ac9
rodrigo <rodrigoandres80@hotmail.com>
2016-03-14 15:48:33 -0500

    Proyecto

--- app/src/main/java/rodrigo/diariodecampo/DCAMPO.java
+++ app/src/main/java/rodrigo/diariodecampo/DCAMPO.java
@@ -1,7 +1,9 @@
 package rodrigo.diariodecampo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 public class DCAMPO extends AppCompatActivity {
@@ -10,10 +12,17 @@ public class DCAMPO extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dcampo);
+    }
+    public void  SIGUIENTE (View SIGUIENTE) {
+        Intent intent = new Intent(this, dcampo1.class);
+        intent.putExtra("diario","si paso este dato");
+        startActivity(intent);
+          }
+    public void  ANTERIOR (View ANTERIOR) {
+        Intent intent = new Intent(this, DCAMPO.class);
+        intent.putExtra("ANTERIOR","si paso este datoANTE");
+        startActivity(intent);
 
-        
 
     }
-
-
 }
--- app/src/main/java/rodrigo/diariodecampo/dcampo1.java
+++ app/src/main/java/rodrigo/diariodecampo/dcampo1.java
@@ -1,7 +1,10 @@
 package rodrigo.diariodecampo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class dcampo1 extends AppCompatActivity {
 
@@ -9,9 +12,17 @@ public class dcampo1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dcampo1);
+        Intent intent = getIntent();
+        String extra = intent.getStringExtra("diario");
+        Toast.makeText(this,"este es el dato " + extra, Toast.LENGTH_LONG ).show();
 
+    }
+    public void  ANTERIOR (View ANTERIOR) {
+        Intent intent = new Intent(this, DCAMPO.class);
+        intent.putExtra("diario","VA PARA ATRAS");
+        startActivity(intent);
 
 
 
     }
-}
+    }
